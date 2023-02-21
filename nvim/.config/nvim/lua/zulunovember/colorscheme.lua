eea04fe46f16206853970f62fdba05509d7ea96c
Eniz Coban <eniz.coban@bitaksi.com>
2023-02-21 15:16:31 +0300

    set background

--- nvim/.config/nvim/lua/zulunovember/colorscheme.lua
+++ nvim/.config/nvim/lua/zulunovember/colorscheme.lua
@@ -7,4 +7,6 @@ endtry
 ]]
 
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
+vim.api.nvim_set_hl(0, "LineNrAbove", { bg = "none" })
+vim.api.nvim_set_hl(0, "LineNrBelow", { bg = "none" })
