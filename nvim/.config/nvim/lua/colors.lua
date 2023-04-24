3347183955e546acd993b7bfdaf93782b4f05e3b
Eniz Coban <eniz.coban@bitaksi.com>
2023-04-24 12:19:42 +0300

    return to sonokai

--- nvim/.config/nvim/lua/colors.lua
+++ nvim/.config/nvim/lua/colors.lua
@@ -1,1 +1,5 @@
-vim.cmd("colorscheme github-colors")
+--vim.g.sonokai_style = "atlantis"
+vim.g.sonokai_style = "andromeda"
+--vim.g.sonokai_style = "maia"
+
+vim.cmd("colorscheme sonokai")
--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -4,7 +4,7 @@ return {
 		lazy = true,
 	},
 	{
-		"lourenci/github-colors",
+		"sainnhe/sonokai",
 		lazy = true,
 	},
 }
